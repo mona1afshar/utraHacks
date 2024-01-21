80fa00b18fe96a7aa6ad124b90a0ce3a81de4132
aria-yang <146586541+aria-yang@users.noreply.github.com>
2024-01-21 09:50:58 -0500

    Update program.ino

--- program/program.ino
+++ program/program.ino
@@ -1,20 +1,20 @@
 //distance sensors
-const int ultraDistTrig = 1;
-const int ultraDistEcho = 0;
+const int ultraDistTrig = 12;
+const int ultraDistEcho = 6;
 
 //Motor Pins
-int EN_A = 11;  //Enable pin for first motor
-int motor1pin1 = 2;    //control pin for first motor
-int motor1pin2 = 3;    //control pin for first motor
+int EN_A = 11;       //Enable pin for first motor
+int motor1pin1 = 2;  //control pin for first motor
+int motor1pin2 = 3;  //control pin for first motor
 
 // Right wheel
-int motor2pin1 = 4;    //control pin for second motor
-int motor2pin2 = 5;    //control pin for second motor
-int EN_B = 10;  //Enable pin for second motor
+int motor2pin1 = 4;  //control pin for second motor
+int motor2pin2 = 5;  //control pin for second motor
+int EN_B = 10;       //Enable pin for second motor
 
 //Initializing variables to store data
-int motor_speed = 150;
-int motor_speed1 = 150;
+int motor_speed; // blue
+int motor_speed1;
 
 
 const int lightDetectorLeft = 7;
@@ -22,12 +22,16 @@ const int lightAnalogLeft = A0;
 const int lightDetectorRight = 8;
 const int lightAnalogRight = A1;
 
-const int led = 13;  //BOTTOM RIGHT
+const int ledRed = 9;
+// const int ledGreen = 13;  //BOTTOM RIGHT
 
 long duration;
 int distanceCm;
 
-const int colour = 140;  //value of black line colour
+const int buttonPin = 13;
+volatile byte buttonReleased = false;
+
+//const int colour = 140;  //value of black line colour
 
 void setup() {
   pinMode(EN_A, OUTPUT);
@@ -38,37 +42,61 @@ void setup() {
   pinMode(EN_B, OUTPUT);
 
   pinMode(ultraDistTrig, OUTPUT);
-  pinMode(ultraDistEcho, INPUT); 
+  pinMode(ultraDistEcho, INPUT);
 
   pinMode(lightAnalogLeft, INPUT);
-  pinMode(lightAnalogRight, INPUT);
-  pinMode(lightDetectorLeft, INPUT);
+  pinMode(lightAnalogLeft, INPUT);
+  pinMode(lightDetectorRight, INPUT);
   pinMode(lightDetectorRight, INPUT);
 
-  pinMode(led, OUTPUT);
+  // pinMode(ledGreen, OUTPUT);
+  // pinMode(ledRed, OUTPUT);
+
+  pinMode(buttonPin, INPUT);
+  attachInterrupt(digitalPinToInterrupt(buttonPin), buttonReleasedInterrupt, FALLING);
 
   Serial.begin(9600);
 }
 
+bool isRed = false;    // 9, Right Side
+bool isGreen = false;  // 13, Left Side
+int value_A0;
+int value_D0;
+
 void loop() {
-  digitalWrite(led, HIGH);
-  // put your main code here, to run repeatedly:   
+
+  value_A0 = analogRead(lightDetectorLeft);   // reads the analog input from the IR distance sensor
+  value_D0 = analogRead(lightDetectorRight);  // reads the digital input from the IR distance sensor
+
+  // put your main code here, to run repeatedly:
   // Blue
-  digitalWrite(motor1pin1, HIGH);
-  digitalWrite(motor1pin2, LOW);
-  analogWrite(EN_A, 0);
+  digitalWrite(motor1pin1, LOW);
+  digitalWrite(motor1pin2, HIGH);
+  analogWrite(EN_A, motor_speed);
   // Black (70% of Blue)
-  digitalWrite(motor2pin1, LOW);
-  digitalWrite(motor2pin2, HIGH);
-  analogWrite(EN_B, 0);
+  digitalWrite(motor2pin1, HIGH);
+  digitalWrite(motor2pin2, LOW);
+  analogWrite(EN_B, motor_speed1);
 
-  delay(1000);
+  detectColour();
 
-  Serial.println(sendForwardUltrasonic());
+  delay(10);
 
+  Serial.println(analogRead(lightAnalogLeft));
+  // Serial.println(analogRead(lightAnalogRight));
+  // Serial.println(digitalRead(buttonPin));
+
+  if(digitalRead(buttonPin)==0){
+    buttonReleased= false;
+    digitalWrite(motor1pin1, LOW);
+    digitalWrite(motor1pin2, LOW);
+    digitalWrite(motor2pin1, LOW);
+    digitalWrite(motor2pin2, LOW);
+    delay(100000);
+  }
 }
 
- int sendForwardUltrasonic() {
+int sendForwardUltrasonic() {
   digitalWrite(ultraDistTrig, LOW);
   delayMicroseconds(2);
   digitalWrite(ultraDistTrig, HIGH);
@@ -79,84 +107,64 @@ void loop() {
   return distanceCm;
 }
 
+void buttonReleasedInterrupt(){
+  buttonReleased=true;
+  // Serial.println("released");
 
-/*void detectColour() {
-  //if front light detector reads white colour move backward, to return onto board
-  if (analogRead(lightDetectorLeft) > 100) {
-    moveLeft();
-  }
-  //if back light detector reads white light, move forward, to return to board
-  else if (analogRead(lightDetectorRight) > colour) {
-    moveForward();
-    digitalWrite(ledFront, LOW);
-    digitalWrite(ledBack, HIGH);
-    digitalWrite(ledLeft, LOW);
-    digitalWrite(ledRight, LOW);
-  }
-  //if left light detector reads white light, move left to return to board
-  else if (analogRead(leftLight) > colour) {
-    moveRight();
-    digitalWrite(ledFront, LOW);
-    digitalWrite(ledBack, LOW);
-    digitalWrite(ledLeft, HIGH);
-    digitalWrite(ledRight, LOW);
-  }
-  //if right light detector reads white light, move left to return to board
-  else if (analogRead(rightLight) > (colour - 10)) {
-    moveLeft();
-    digitalWrite(ledFront, LOW);
-    digitalWrite(ledBack, LOW);
-    digitalWrite(ledLeft, LOW);
-    digitalWrite(ledRight, HIGH);
-  }
 }
-//sends an ultrasound burst out of the front sensor
-int sendForwardUltrasonic() {
-  digitalWrite(ultraDistTrig, LOW);
-  delayMicroseconds(2);
-  digitalWrite(ultraDistTrig, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(frontDistTrig, LOW);
-  duration = pulseIn(frontDistEcho, HIGH);
-  distanceCm = duration * 0.034 / 2;
-  return distanceCm;
+
+void detectColour() {
+  // if light detector left reads white, move forward
+  if (analogRead(lightDetectorLeft) > 850) {
+    moveLeft();
+  }
+  //if light detector right reads white, move forward, to return to board
+  else if (analogRead(lightDetectorRight) > 750) {
+    moveRight();
+  }
+  // WORRY ABOUT CIRCLE LATER
+  //otherwise move forward
+  else {
+    moveForward();
+  }
 }
 
 //moves foward
 void moveForward() {
-  analogWrite(rightMotorForward, 100);
-  analogWrite(leftMotorForward, 140);
-  analogWrite(leftMotorBackward, 0);
-  analogWrite(rightMotorBackward, 0);
+  motor_speed = 100;   // Blue wheel
+  motor_speed1 = 100;  // Black wheel
+  digitalWrite(ledRed, LOW);
+  // digitalWrite(ledGreen, LOW);
 }
 
 //moves backward
-void moveBackward() {
+/* void moveBackward() {
   analogWrite(leftMotorForward, 0);
   analogWrite(rightMotorForward, 0);
   analogWrite(leftMotorBackward, 255);   //160
   analogWrite(rightMotorBackward, 255);  //150
-}
+} */
 
 //turns right
 void moveRight() {
-  analogWrite(leftMotorForward, 255);  //was 100
-  analogWrite(rightMotorForward, 0);
-  analogWrite(leftMotorBackward, 0);
-  analogWrite(rightMotorBackward, 255);  //was 90
+  motor_speed = 80;
+  motor_speed1 = 0;
+  // digitalWrite(ledGreen, HIGH);
+  digitalWrite(ledRed, LOW);
 }
 //turns left
 void moveLeft() {
-  analogWrite(leftMotorForward, 0);
-  analogWrite(rightMotorForward, 255);  //was 77
-  analogWrite(leftMotorBackward, 255);  //was 100
-  analogWrite(rightMotorBackward, 0);
+
+  motor_speed = 0;
+  motor_speed1 = 80;
+  digitalWrite(ledRed, HIGH);
+  // digitalWrite(ledGreen, LOW);
 }
 
 //stops all motors
-void stopAll() {
+/* void stopAll() {
   analogWrite(leftMotorForward, 0);
   analogWrite(rightMotorForward, 0);
   analogWrite(leftMotorBackward, 0);
   analogWrite(rightMotorBackward, 0);
-}*/
+*/
